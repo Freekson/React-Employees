cf31fb767a271b90cad296f81eef6220ff803d39
Yehor Dreval <freeksons@gmail.com>
2023-08-23 21:48:20 +0200

    add styles, paths, types and router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,20 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+
+import { Path } from "./paths";
+import { Login } from "./pages/login";
+
 import "./App.scss";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path={Path.home} element={<h1>Hello world</h1>} />
+        <Route path={Path.login} element={<Login />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
